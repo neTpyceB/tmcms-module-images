81f11a205321637de81147f8447257c2df50a25b
Vadims Petrusevs <pvman@inbox.ru>
2017-11-19 14:19:06 +0100

    Const added for index column

--- src/Entity/ImageEntity.php
+++ src/Entity/ImageEntity.php
@@ -9,14 +9,14 @@ use TMCms\Orm\Entity;
  * @package TMCms\Modules\Images\Entity
  * @method string getImage()
  * @method string getItemType()
- * @method int getItemId()
- * @method int getOrder()
+ * @method string getItemId()
+ * @method string getOrder()
  *
- * @method $this setActive(int $flag)
+ * @method $this setActive(string $flag)
  * @method $this setImage(string $image)
  * @method $this setItemType(string $type)
- * @method $this setItemId(int $id)
- * @method $this setOrder(int $order)
+ * @method $this setItemId(string $id)
+ * @method $this setOrder(string $order)
  */
 class ImageEntity extends Entity
 {
--- src/Entity/ImageEntityRepository.php
+++ src/Entity/ImageEntityRepository.php
@@ -1,38 +1,59 @@
 <?php
+declare(strict_types=1);
 
 namespace TMCms\Modules\Images\Entity;
 
+use TMCms\Orm\Entity;
 use TMCms\Orm\EntityRepository;
+use TMCms\Orm\TableStructure;
 
 /**
  * Class ImageRepository
  * @package TMCms\Modules\Images\Entity
  *
- * @method $this setWhereActive(int $flag)
+ * @method $this setWhereActive(int|string $flag)
  * @method $this setWhereImage(string $image_path)
- * @method $this setWhereItemType(string $type)
- * @method $this setWhereItemId(int $id)
+ * @method $this setWhereItemId(string $id)
  */
 class ImageEntityRepository extends EntityRepository
 {
+    const FIELD_ITEM_TYPE = 'item_type';
+
     protected $table_structure = [
         'fields' => [
             'item_id' => [
-                'type' => 'index',
+                'type' => TableStructure::FIELD_TYPE_INDEX,
             ],
-            'item_type' => [
-                'type' => 'varchar',
+            self::FIELD_ITEM_TYPE => [
+                'type' => TableStructure::FIELD_TYPE_VARCHAR_255,
             ],
             'image' => [
-                'type' => 'varchar',
+                'type' => TableStructure::FIELD_TYPE_VARCHAR_255,
             ],
             'order' => [
-                'type' => 'int',
-                'unsigned' => true,
+                'type' => TableStructure::FIELD_TYPE_UNSIGNED_INTEGER,
             ],
             'active' => [
-                'type' => 'bool',
+                'type' => TableStructure::FIELD_TYPE_BOOL,
             ],
         ],
     ];
+
+    /**
+     * @param string|Entity|EntityRepository $type
+     */
+    public function setWhereItemType($type) {
+        // If Entity or EntityRepository - get string value from it
+        if (is_a($type, Entity::class)) {
+            /** @var Entity $type */
+            $type = $type->getUnqualifiedShortClassName();
+        }
+        // If EntityRepository - get string value from it
+        if (is_a($type, EntityRepository::class)) {
+            /** @var Entity $type */
+            $type = str_replace(EntityRepository::CLASS_RELATION_NAME, '', $type->getUnqualifiedShortClassName());
+        }
+
+        $this->addSimpleWhereField(self::FIELD_ITEM_TYPE, $type);
+    }
 }
